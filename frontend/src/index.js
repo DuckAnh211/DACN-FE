377e8420179d35856071b7af6d46e97ee27d1ac5
22520160 <163868527+22520160@users.noreply.github.com>
2025-04-25 14:47:44 +0700

    Delete frontend directory

--- frontend/src/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Đảm bảo App.js tồn tại trong src
-
-ReactDOM.render(<App />, document.getElementById('root'));
